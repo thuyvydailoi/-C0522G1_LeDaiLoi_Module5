91dc0ac92a5cdcd03ca6a2ccf0095d94a57e795b
unknown <you@example.com>
2022-10-19 14:18:11 +0700

    update

--- ss04_component/exercise/angular_navbar_footer/src/app/app.module.ts
+++ ss04_component/exercise/angular_navbar_footer/src/app/app.module.ts
@@ -8,6 +8,7 @@ import {FormsModule} from "@angular/forms";
 import { LikeComponent } from './like/like.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { FooterComponent } from './footer/footer.component';
     ArticleComponent,
     LikeComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
     imports: [
         BrowserModule,
