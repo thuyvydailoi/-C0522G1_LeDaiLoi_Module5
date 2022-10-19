5d113c81822079add1a6d2dfce08b31b5c19c794
unknown <you@example.com>
2022-10-19 07:59:40 +0700

    update

--- ss04_component/exercise/angular_navbar_footer/e2e/src/app.e2e-spec.ts
+++ ss04_component/exercise/angular_navbar_footer/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angulalr-navbar-footer app is running!');
+    expect(page.getTitleText()).toEqual('angular-hacker-app app is running!');
   });
 
   afterEach(async () => {
--- ss04_component/exercise/angular_navbar_footer/src/app/app.component.spec.ts
+++ ss04_component/exercise/angular_navbar_footer/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angulalr-navbar-footer'`, () => {
+  it(`should have as title 'angular-hacker-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angulalr-navbar-footer');
+    expect(app.title).toEqual('angular-hacker-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angulalr-navbar-footer app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-hacker-app app is running!');
   });
 });
